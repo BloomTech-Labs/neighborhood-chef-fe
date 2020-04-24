881f61d29cc3cf2f311296e690927b28ac6a4cce
Kyle Richardson <kyle.richardson1@gmail.com>
2020-04-23 20:22:41 -0500

    switched all icons to iconify to match figma

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -8,36 +8,44 @@ import AddBoxOutlinedIcon from "@material-ui/icons/AddBoxOutlined";
 import BookOutlinedIcon from "@material-ui/icons/BookOutlined";
 import SmsOutlinedIcon from "@material-ui/icons/SmsOutlined";
 import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+import { Icon, InlineIcon } from "@iconify/react";
+import houseDoor from "@iconify/icons-bi/house-door";
+import calendarOutlined from "@iconify/icons-ant-design/calendar-outlined";
+import outlineNotifications from "@iconify/icons-ic/outline-notifications";
+import calendarPlus from "@iconify/icons-mdi/calendar-plus";
+import bookIcon from "@iconify/icons-jam/book";
+import outlineTextsms from "@iconify/icons-ic/outline-textsms";
+import outlineSettings from "@iconify/icons-ic/outline-settings";
 
 const Sidebar = () => {
   return (
     <nav className="sidebar-container">
       <div>
-        <HouseOutlinedIcon />
+        <Icon icon={houseDoor} />
         <Link to="/view-events">Your Neighborhood</Link>
       </div>
       <div>
-        <EventOutlinedIcon />
+        <Icon icon={calendarOutlined} />
         <Link to="/calendar">Calendar</Link>
       </div>
       <div>
-        <NotificationsNoneOutlinedIcon />
+        <Icon icon={outlineNotifications} />
         <Link to="/notifications">Notifications</Link>
       </div>
       <div>
-        <AddBoxOutlinedIcon />
+        <Icon icon={calendarPlus} />
         <Link to="/create-event">Create Event</Link>
       </div>
       <div>
-        <BookOutlinedIcon />
+        <Icon icon={bookIcon} />
         <Link to="/shared-recipes">Shared Recipes</Link>
       </div>
       <div>
-        <SmsOutlinedIcon />
+        <Icon icon={outlineTextsms} />
         <Link to="/messages">Messages</Link>
       </div>
       <div>
-        <SettingsOutlinedIcon />
+        <Icon icon={outlineSettings} />
         <Link to="/settings">Settings</Link>
       </div>
     </nav>
