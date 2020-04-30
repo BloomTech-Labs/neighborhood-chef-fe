55c9befbf58b9cf2b2ed5f462bf4230fdd2ba234
Kyle Richardson <kyle.richardson1@gmail.com>
2020-04-29 21:08:42 -0500

    merge from master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 import Register from "./components/account/Register.js";
 import CreateEvent from "./components/events/CreateEvent";
 import ViewEvents from "./components/events/ViewEvents";
