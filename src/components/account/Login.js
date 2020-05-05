bba37ef5dc0e254668e1f49af85bdf244eee0f0c
Kyle Richardson <kyle.richardson1@gmail.com>
2020-05-04 23:01:17 -0500

    basic login button for okta

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { buttonStyles } from "../../styles";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Login Component</h1>;
+  const classes = buttonStyles();
+  return (
+    <div className="login-container">
+      <h1>Neighborhood Chef</h1>
+      <p style={{ fontStyle: "italic" }}>Prepare to eat well</p>
+      <div className={`${classes.root} ${classes.single}`}>
+        Login Securely with Okta
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        Don't have an account? <Link to="/register">Register now.</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
