815825cf96778ee1f5d625797d586bb71a2e140a
patrick-replogle <patrickr1138@gmail.com>
2020-05-06 16:43:22 -0700

    disabled prettier and restored double quotes that had been overwritten by single quotes.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/account/Register.js";
-import CreateEvent from './components/events/create-event/CreateEvent';
+import CreateEvent from "./components/events/create-event/CreateEvent";
 import ViewEvents from "./components/events/ViewEvents";
 import Dashboard from "./components/dashboard/Dashboard";
 import Settings from "./components/other/Settings";
@@ -17,7 +17,7 @@ function App() {
     <div className="app-container">
       <Routes>
         {/*redirect depricated  in react router v6. need to use server side redirect most likely*/}
-        <Route path="/" element={<Dashboard />} />{' '}
+        <Route path="/" element={<Dashboard />} />{" "}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/create-event" element={<CreateEvent />} />
         <Route path="/view-events" element={<ViewEvents />} />
