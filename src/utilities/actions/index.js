f8aa4421e548bfcbadb376c183aedc2747a39e49
Brennan Neilson <bvneilson@gmail.com>
2020-05-05 19:07:13 -0600

    attempting to fix merge conflicts

--- src/utilities/actions/index.js
+++ src/utilities/actions/index.js
@@ -5,6 +5,9 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const INCREMENT = "INCREMENT";
 export const CHANGE_PAGE = "CHANGE_PAGE";
+export const MAKEACTIVE = "MAKEACTIVE";
+export const CHANGE_MONTH = "CHANGE_MONTH";
+export const RSVP = "RSVP";
 
 //action using thunk middleware, NOT hooks
 export const login = (event, credentials) => (dispatch) => {
@@ -29,3 +32,24 @@ export const increment = () => ({
 export const changePage = () => ({
   type: CHANGE_PAGE,
 });
+
+export const makeActive = (id) => ({
+  type: MAKEACTIVE,
+  payload: id,
+});
+
+export const setMonth = (type) => ({
+  type: CHANGE_MONTH,
+  payload: type,
+});
+
+export const rsvp = (event, id) => {
+  event.preventDefault();
+  return {
+    type: RSVP,
+    payload: {
+      name: event.target.innerHTML,
+      id: id,
+    },
+  };
+};
