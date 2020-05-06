b282898ffe3d4790eb3439c146ffd6dfb4541e09
Aaroneld <merrifield48@gmail.com>
2020-05-06 00:32:40 -0400

    Okta sign-in integration for front-end
    DEPENDENCIES CHANGED:
    
    react-router v6alpha => v5.1.2
    react-router-dom v6alpha => v5.1.2
    
    DEPENDENCIES ADDED
    crypto-broswerify
    nanoid
    local-storage
    querystring
    
    COMPONENTS ADDED
    (all in ./src/components/util)
    generic-redirect.jsx
    login-redirect.jsx
    private-route.jsx
    
    Dashboard route made private
    changed react-router syntax to v5 acceptable
    add onClick handler in /login for okta login redirect

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { buttonStyles } from "../../styles";
+import ls from 'local-storage';
+import { nanoid } from 'nanoid';
+
+const qs = require('querystring');
+const crypto = require('crypto-browserify');
 
 const Login = () => {
-  return <h1>Login Component</h1>;
+  const classes = buttonStyles();
+
+  const handleClick = e => {
+    e.preventDefault();
+    ls.set('code_verifier', nanoid(43) );
+
+    const hash = crypto.createHash('sha256')
+                        .update(ls.get("code_verifier"))
+                        .digest('base64')
+                        .replace(/\+/g, '-')
+                        .replace(/\//g, '_')
+                        .replace(/=/g, '');  
+    const query = {
+        client_id: "0oa9qxckmhGd2aLsZ4x6",
+        response_type: "code",
+        scope: "openid",
+        redirect_uri: "http://localhost:3000/login-redirect-url",
+        state: "state-bsaWCD8F0dkd85REyU87",
+        code_challenge_method: "S256",
+        code_challenge: hash
+
+    }
+
+    const redirectURL = `https://dev-599411.okta.com/oauth2/default/v1/authorize?${qs.stringify(query)}`;
+    console.log(hash);
+    window.location.replace(`${redirectURL}`);
+}
+
+  return (
+    <div className="login-container">
+      <h1>Neighborhood Chef</h1>
+      <p style={{ fontStyle: "italic" }}>Prepare to eat well</p>
+
+      {/*need to add proper okta url for login here */}
+        {" "}
+        <div className={`${classes.root} ${classes.single}`} onClick={handleClick}>
+          {" "}
+          Login Securely with Okta
+        </div>
+      <div style={{ marginTop: "10px" }}>
+        Don't have an account?{" "}
+        <span className="green-link">
+          <Link to="/register">Register now.</Link>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/utils/generic-redirect.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import ls from 'local-storage';
+
+function GenericRedirect (props){
+
+    const { push } = useHistory();
+    const { redirect_path } = useParams();
+
+    if(!ls.get('access_token')) { push(`/generic-redirect/${redirect_path}`); }
+    else { push(`/${redirect_path}`); }
+
+    return null
+}
+
+export default GenericRedirect
