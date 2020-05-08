5ff8807d59f9f2771ce8c07ae82419707fa1a1e8
Brennan Neilson <bvneilson@gmail.com>
2020-05-07 20:09:31 -0600

    updating test so that it doesn't fail

--- src/components/account/Register.test.js
+++ src/components/account/Register.test.js
@@ -1,15 +1,29 @@
 import React from "react";
 import Register from "./Register.js";
 import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "../../utilities/reducers";
+import thunk from "redux-thunk";
+
 import "@testing-library/jest-dom/extend-expect";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 describe("Test register static properties", () => {
   let RegisterComponent;
   beforeEach(() => {
-    RegisterComponent = render(<Register />);
+    RegisterComponent = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Register />
+        </BrowserRouter>
+      </Provider>
+    );
   });
 
   test("Register component renders", () => {
-    expect(RegisterComponent.getByText(/First Name/i));
+    expect(RegisterComponent.getByText(/Sign up below/i));
   });
 });
