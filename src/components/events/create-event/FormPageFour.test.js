6d5f1af2928c1168ffdc6faca264cfa798ed86b9
patrick-replogle <patrickr1138@gmail.com>
2020-05-13 11:04:11 -0700

    added some tests for components that were missing them. still might break out more files into seperate components

---
+++ src/components/events/create-event/FormPageFour.test.js
@@ -0,0 +1,58 @@
+import React from "react";
+import FormPageFour from "./FormPageFour.js";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+const values = {
+  title: "BBQ",
+  address: "123 ABC St.",
+  date: new Date(),
+  startTime: "6:00pm",
+  endTime: "9:00pm",
+};
+
+const userList = [
+  { id: 1, firstName: "Homer", lastName: "Simpon", email: "homer@gmail.com" },
+  { id: 3, firstName: "Bart", lastName: "Simpson", email: "bart@gmail.com" },
+];
+
+const inviteList = [
+  {
+    id: 4,
+    firstName: "Lisa",
+    lastName: "Simpson",
+    email: "list@gmail.com",
+  },
+  {
+    id: 5,
+    firstName: "Ralph",
+    lastName: "Wiggum",
+    email: "ralph@gmail.com",
+  },
+];
+
+const mockStore = configureStore([]);
+
+describe("Test FormPageFour component", () => {
+  let FormPageFourComponent;
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      userList: userList,
+      inviteList: inviteList,
+      newEvent: values,
+    });
+    FormPageFourComponent = render(
+      <Provider store={store}>
+        <FormPageFour />
+      </Provider>
+    );
+  });
+
+  test("FormPageFour component renders", () => {
+    expect(FormPageFourComponent).toBeDefined();
+    expect(FormPageFourComponent.getByText(/BBQ/i));
+  });
+});
---
+++ src/components/events/create-event/SearchFriends.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import "@testing-library/jest-dom/extend-expect";
+
+import SearchFriends from "./SearchFriends.js";
+
+const event = {
+  title: "BBQ",
+  address: "123 ABC St.",
+  date: new Date(),
+  startTime: "6:00pm",
+  endTime: "9:00pm",
+};
+
+const userList = [
+  { id: 1, firstName: "Homer", lastName: "Simpon", email: "homer@gmail.com" },
+  { id: 3, firstName: "Bart", lastName: "Simpson", email: "bart@gmail.com" },
+];
+
+const inviteList = [
+  {
+    id: 4,
+    firstName: "Lisa",
+    lastName: "Simpson",
+    email: "list@gmail.com",
+  },
+  {
+    id: 5,
+    firstName: "Ralph",
+    lastName: "Wiggum",
+    email: "ralph@gmail.com",
+  },
+];
+
+const mockStore = configureStore([]);
+
+describe("Test SearchFriends component", () => {
+  let SearchFriendsComponent;
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      userList: userList,
+      inviteList: inviteList,
+      event: event,
+    });
+
+    SearchFriendsComponent = render(
+      <Provider store={store}>
+        <SearchFriends filteredList={userList} />
+      </Provider>
+    );
+  });
+
+  test("SearchFriendsComponent renders", () => {
+    expect(SearchFriendsComponent).toBeDefined();
+    expect(SearchFriendsComponent.getByText(/Invite Neighbors/i));
+  });
+});
