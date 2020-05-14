e3e10731f62dab1167cef85823289b1ea8497f95
patrick-replogle <patrickr1138@gmail.com>
2020-05-14 09:14:07 -0700

    changed single quotes to double to conform to rest of component in event card

--- src/components/events/create-event/EventCard.js
+++ src/components/events/create-event/EventCard.js
@@ -39,9 +39,9 @@ const EventCard = () => {
 
         <div
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
             marginLeft: "30px",
             textAlign: "left",
             width: "50%",
@@ -63,7 +63,7 @@ const EventCard = () => {
               flexDirection: "column",
             }}
           >
-            <p style={{ color: "rgba(0, 0, 0, 0.35)", marginBottom: '22px' }}>
+            <p style={{ color: "rgba(0, 0, 0, 0.35)", marginBottom: "22px" }}>
               {values.date && moment(Number(values.date)).format("MMM Do YYYY")}
               .&nbsp;
             </p>
