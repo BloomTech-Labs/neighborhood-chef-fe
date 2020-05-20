28cf4580df5e6ae919d88d3058c93bf36831dcb7
Kyle Richardson <kyle.richardson1@gmail.com>
2020-05-19 22:37:58 -0500

    added function globally, sorting calendar

--- src/utilities/functions/index.js
+++ src/utilities/functions/index.js
@@ -18,3 +18,41 @@ export const timeAgo = (dateCreated) => {
   if (configuredTime !== 1) showType = showType + "s";
   return `${configuredTime} ${showType} ago`;
 };
+
+export const parseTime = (date, start, end) => {
+  var options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = new Date(parseInt(date)); //formats 13 digit UNIX date provided by database
+  const simplifiedDate = formattedDate.toLocaleDateString("en-us", options); // reduces to just YYYY MM, DD format
+  const addStartTime = new Date(`${simplifiedDate} ${start}`); // creates new date using start_time value for time, instead of 00:00:00 default
+  let displayedEndTime, displayedStartTime;
+  if (!!end) {
+    const addEndTime = new Date(`${simplifiedDate} ${end}`);
+    displayedEndTime = addEndTime
+      .toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "2-digit",
+      })
+      .toLowerCase(); //formatting just time in 12 hr format with lower case am pm
+  }
+  displayedStartTime = addStartTime
+    .toLocaleTimeString([], {
+      hour: "numeric",
+      minute: "2-digit",
+    })
+    .toLowerCase();
+  const getWeekday = addStartTime.toLocaleDateString("en-us", {
+    weekday: "short",
+  });
+  const getDay = addStartTime.toLocaleDateString("en-us", {
+    day: "numeric",
+  });
+
+  return {
+    formattedDate: simplifiedDate,
+    weekday: getWeekday,
+    day: getDay,
+    startTime: displayedStartTime,
+    endTime: displayedEndTime,
+    unixStart: addStartTime.getTime(),
+  };
+};
