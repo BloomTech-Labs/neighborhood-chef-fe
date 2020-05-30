893d6ad71b167ef1ce9b0ae7dafa0bbdbcb49e9c
Kyle Richardson <kyle.richardson1@gmail.com>
2020-05-30 13:06:22 -0500

    added bearer to axios withauth

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -7,7 +7,7 @@ export const axiosWithAuth = () => {
   return axios.create({
     headers: {
       "Content-Type": "application/json",
-      Authorization: `${token}`,
+      Authorization: `Bearer ${token}`,
     },
   });
 };
