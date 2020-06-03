30b156f6cff57ad762285334871bb3664eefff3d
Aaroneld <merrifield48@gmail.com>
2020-06-02 22:10:39 -0400

    Commiting changes that came from the pull from master

---
+++ src/utilities/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import ls from "local-storage";
+
+export const axiosWithAuth = () => {
+  const token = ls.get("access_token");
+
+  return axios.create({
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
