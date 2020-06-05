138932db2afb02518b5342230e6b2e709bbb2774
Aaroneld <merrifield48@gmail.com>
2020-06-05 13:38:53 -0400

    removed unneccesary imports in dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,8 @@
 import React, { useEffect } from "react";
-import Sidebar from "./Sidebar";
 // import Feed from "./Feed";
 import RecentEvents from "./RecentEvents";
 import Header from "./Header";
-import ls from "local-storage";
-import jwt from "jwt-decode";
-import { USER_BY_EMAIL } from "../../graphql/users/user-queries";
-import { axiosWithAuth } from "../../utilities/axiosWithAuth";
-import { print } from "graphql";
+
 
 const Dashboard = () => {
 
