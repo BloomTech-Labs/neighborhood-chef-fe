69e334b666b6d35740bae9a7ce8ae718164b6e99
Aaroneld <merrifield48@gmail.com>
2020-06-05 20:14:15 -0400

    Revert "removed unneccesary imports in dashboard component"
    This reverts commit 138932db2afb02518b5342230e6b2e709bbb2774.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,13 @@
 import React, { useEffect } from "react";
+import Sidebar from "./Sidebar";
 // import Feed from "./Feed";
 import RecentEvents from "./RecentEvents";
 import Header from "./Header";
-
+import ls from "local-storage";
+import jwt from "jwt-decode";
+import { USER_BY_EMAIL } from "../../graphql/users/user-queries";
+import { axiosWithAuth } from "../../utilities/axiosWithAuth";
+import { print } from "graphql";
 
 const Dashboard = () => {
   useEffect(() => {
