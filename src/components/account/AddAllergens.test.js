5c7d3e701face1c133d0ee46bafee0b2d66c679e
patrick-replogle <patrickr1138@gmail.com>
2020-06-18 11:54:29 -0700

    updated register advanced options tests to remove formik warnings and also deleted extra console logs that were clogging up test output

--- src/components/account/AddAllergens.test.js
+++ src/components/account/AddAllergens.test.js
@@ -1,12 +1,17 @@
 import React from "react";
 import AddAllergens from "./AddAllergens.js";
+import { Formik } from "formik";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 describe("Test AddAllergens component", () => {
   let AddAllergensComponent;
   beforeEach(() => {
-    AddAllergensComponent = render(<AddAllergens values={{ allergens: [] }} />);
+    AddAllergensComponent = render(
+      <Formik>
+        <AddAllergens values={{ allergens: [] }} />
+      </Formik>
+    );
   });
 
   test("AddAllergens component renders", () => {
