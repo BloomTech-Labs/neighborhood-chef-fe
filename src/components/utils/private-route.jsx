6c161f2e068d201a556b90f49c3dd83598b27dbf
patrick-replogle <patrickr1138@gmail.com>
2021-02-23 16:44:38 -0800

    bypassing okta for now

--- src/components/utils/private-route.jsx
+++ src/components/utils/private-route.jsx
@@ -1,41 +1,56 @@
-import React, { useState, useEffect } from "react";
-import { useHistory, Route } from "react-router-dom";
-import ls from "local-storage";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useHistory, Route } from 'react-router-dom';
+import { axiosWithAuth } from '../../utilities/axiosWithAuth';
+import ls from 'local-storage';
+import axios from 'axios';
 
-const qs = require("querystring");
+const qs = require('querystring');
+
+//const PrivateRoute = ({ component: Component, ...rest }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) => {
+//         if () {
+//           return <Component {...props} />;
+//         } else {
+//           return <Redirect to="/login" />;
+//         }
+//       }}
+//     />
+//   );
+// };
 
 function PrivateRoute({ component: Component, path, ...props }) {
-  const [finalComponent, setFinalComponent] = useState(null);
-
+  const [response, setResponse] = useState(null);
+  const [requestMade, setRequestMade] = useState(false);
   const { push } = useHistory();
 
   useEffect(() => {
     const validate = async () => {
+      let res;
       try {
-        if (ls.get("access_token")) {
-          const body = {
-            token: ls.get("access_token"),
-            token_type_hint: "access_token",
-            client_id: process.env.REACT_APP_CLIENT_ID,
-          };
-
-          const response = await axios.post(
-            "https://dev-36383529.okta.com/oauth2/default/v1/introspect",
-            qs.stringify(body)
-          );
-
-          if (
-            response.status.toString().match(/2[0-9][0-9]/) &&
-            response.data.active
-          ) {
-            setFinalComponent(
-              <Route {...props} path={path} component={Component} />
+        if (ls.get('access_token')) {
+          if (!process.env.REACT_APP_FRONT_END_BASE_URL.includes('localhost')) {
+            const body = {
+              token: ls.get('access_token'),
+              token_type_hint: 'access_token',
+              client_id: process.env.REACT_APP_CLIENT_ID,
+            };
+            res = await axios.post(
+              'https://dev-36383529.okta.com/oauth2/default/v1/introspect',
+              qs.stringify(body)
             );
+            setResponse(res);
           } else {
-            push("/");
+            res = await axiosWithAuth().post(
+              'http://localhost:5100/authenticate',
+              {}
+            );
+            setResponse(res);
           }
-        } else push("/");
+          setRequestMade(true);
+        }
       } catch (err) {
         console.dir(err);
       }
@@ -45,7 +60,19 @@ function PrivateRoute({ component: Component, path, ...props }) {
     // eslint-disable-next-line
   }, [path, Component]);
 
-  return finalComponent;
+  if (!response) {
+    return <p>...loading</p>;
+  }
+  if (response.status.toString().match(/2[0-9][0-9]/) && response.data.active) {
+    return <Route {...props} path={path} component={Component} />;
+  }
+  if (response.data.success) {
+    return <Route {...props} path={path} component={Component} />;
+  }
+  if (!response.data.success && requestMade) {
+    push('/');
+  }
+  return null;
 }
 
 export default PrivateRoute;
